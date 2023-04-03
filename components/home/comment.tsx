e55f5730a11394e8114201a5927faa2bfd0642a5
dxtobi <akanbijosephtobi@gmail>
2023-04-03 01:09:37 -0700

    cant be empty

--- components/home/comment.tsx
+++ components/home/comment.tsx
@@ -20,13 +20,13 @@ export default function Comment(params: { data: any; }) {
 
     console.log("data::", data)
     return (
-        <div className={` break-words  w-full whitespace-normal ${data.accepted? "bg-[#30343a] box-shadow text-[#ffffff]":"bg-[#ffffff] text-[#30343a]"}  mb-3 p-5 rounded-lg flex items-start gap-2`}>
+        <div className={` w-full  ${data.accepted? "bg-[#30343a] box-shadow text-[#ffffff]":"bg-[#ffffff] text-[#30343a]"}  mb-3 p-5 rounded-lg flex items-start gap-2`}>
             <Image src={data?.user?.image} alt='' width={50} height={50} className=' rounded-full' />
             <div className="flex flex-col">
                 <div className="w-full break-words break-all font-bold  overflow-w">{data?.user?.name}</div>
-                <div>{data?.message}</div>
+                <div className="break-words break-all whitespace-normal w-full overflow-w">{data?.message}</div>
                 <div className=" w-full h-[1px] bg-slate-500 my-3"></div>
-                <div className=" text-sm">2023 20 1</div>
+                <div className=" text-sm">2023 20 1 {data.created_at}</div>
             </div>
         </div>
     );
