443bee5952a159826365668b332a5b72c02c8fe6
dxtobi <akanbijosephtobi@gmail>
2023-04-19 14:36:43 -0700

    added copy func.

--- components/cards/SyntexHylighter.tsx
+++ components/cards/SyntexHylighter.tsx
@@ -1,8 +1,9 @@
 import hljs from 'highlight.js';
 import 'highlight.js/styles/github.css';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 import { BsClipboardPlus, BsDownload, BsFillSunFill } from 'react-icons/bs';
+import { elementToPNG } from '../../lib/utils';
 
 
 const codeString = `export default function SyntexHilight({code}) {
@@ -14,23 +15,62 @@ const codeString = `export default function SyntexHilight({code}) {
       </SyntaxHighlighter>
     );
   };`;
+  
 
-
-export default function SyntexHilight({ code }) {
+export default function SyntexHilight(code: any) {
     const highlightedCode = hljs.highlight(codeString, { language: 'typescript' }).value
     const [theme, setTheme] = useState(true)
 
 
+    const [copied, setCopied] = useState(false);
+
+    const ref = useRef<HTMLDivElement>(null);
+
+    function handleConvertToPNG() {
+    if (ref.current) {
+        elementToPNG(ref.current)
+        .then(() => {
+            console.log('Image downloaded successfully.');
+        })
+        .catch(error => {
+            console.error('Failed to download image:', error);
+        });
+    }
+    }
+
+    
+  const copyToClipboard = (text: string): void => {
+    const textarea = document.createElement('textarea');
+    textarea.value = text;
+    textarea.setAttribute('readonly', '');
+    textarea.style.position = 'absolute';
+    textarea.style.left = '-9999px';
+    document.body.appendChild(textarea);
+    const selected = document.getSelection()?.rangeCount != undefined
+        ? document.getSelection()?.getRangeAt(0)
+        : false;
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+    if (selected) {
+      document.getSelection()?.removeAllRanges();
+      document.getSelection()?.addRange(selected);
+    }
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 3000);
+  };
 
 
     return (
-        <div>
+        <div ref={ref}>
             <div className='flex justify-between items-center my-2 gap-2'>
                 <button onClick={()=>setTheme(!theme)} className='flex items-center justify-center font-2xl p-2 w-full bg-[#081822] text-white rounded-lg'><BsFillSunFill /></button>
-                <button className='flex items-center justify-center font-2xl p-2 w-full bg-[#081822] text-white rounded-lg'><BsClipboardPlus /></button>
-                <button className='flex items-center justify-center font-2xl p-2 w-full bg-[#081822] text-white rounded-lg'><BsDownload /></button>
+                <button className={`flex items-center justify-center font-2xl p-2 w-full ${copied?"bg-[#00c50c]":"bg-[#081822]"}  text-white rounded-lg`} onClick={()=>copyToClipboard(codeString)}><BsClipboardPlus /></button>
+                <button onClick={handleConvertToPNG} className='flex items-center justify-center font-2xl p-2 w-full bg-[#081822] text-white rounded-lg'><BsDownload /></button>
             </div>
-            <pre className={`overflow-x-scroll ${theme?"bg-[#d4d4d4]":"bg-[#081822]"} rounded-lg p-4`}>
+            <pre  className={`overflow-x-scroll ${theme?"bg-[#d4d4d4]":"bg-[#081822]"} rounded-lg p-4 w-full`}>
                 <code >
                     <div  dangerouslySetInnerHTML={{ __html: highlightedCode }} />
                 </code>
---
+++ lib/utils.ts
@@ -0,0 +1,30 @@
+import html2canvas from 'html2canvas';
+import { saveAs } from 'file-saver';
+
+export async function elementToPNG(element: HTMLElement): Promise<void> {
+    try {
+
+        const height = element.scrollHeight;
+        const width = element.scrollWidth;
+      const canvas = await html2canvas(element, {
+        height, width
+      });
+      const blob = await new Promise<Blob | null>((resolve, reject) => {
+        canvas.toBlob(blob => {
+          if (blob) {
+            resolve(blob);
+          } else {
+            reject(new Error('Failed to create image blob.'));
+          }
+        }, 'image/png');
+      });
+      if (blob) {
+        saveAs(blob, 'image.png');
+      } else {
+        throw new Error('Failed to download image: Blob is null.');
+      }
+    } catch (error) {
+      console.error('Failed to download image:', error);
+      throw error;
+    }
+  }
